2210e519886e630cd4a0bf3afed85507556e7b60
Aman Sharma <shrma.aman7@gmail.com>
2022-05-19 12:37:41 +0530

    Initial Commit

---
+++ Developer Cards/Developer_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Developer_CardsApp.swift
+//  Developer Cards
+//
+//  Created by AMAN SHARMA on 19/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Developer_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
